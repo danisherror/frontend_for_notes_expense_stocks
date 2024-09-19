74a46e76f93416b4b097b27ecf5502a5320dd6c0
DANISH MAHAJAN <77108977+danisherror@users.noreply.github.com>
2024-09-19 14:58:50 +0530

    transaction popup view done

--- src/Transactions/Show_All_Transactions.jsx
+++ src/Transactions/Show_All_Transactions.jsx
@@ -103,7 +103,7 @@ const Show_All_Transactions = () => {
             case "Low":
                 return 'bg-secondary text-secondary';
             default:
-                return 'bg-info text-info';
+                return 'bg-primary text-primary';
         }
     }
 
@@ -122,10 +122,13 @@ const Show_All_Transactions = () => {
                         <thead>
                             <tr className="bg-gray-2 text-left dark:bg-meta-4">
                                 <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
-                                Transaction Type
+                                    Transaction Type
                                 </th>
                                 <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
                                     Second Party
+                                </th>
+                                <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                                    View
                                 </th>
                                 <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
                                     Edit
@@ -146,13 +149,15 @@ const Show_All_Transactions = () => {
                                     </td>
                                     <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
                                         <h5 className="font-medium text-black dark:text-white">
-                                            {transaction.transaction_date}
-                                        </h5>
-                                    </td>
-                                    <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
-                                        <h5 className="font-medium text-black dark:text-white">
                                             {transaction.second_party}
                                         </h5>
+                                    </td>
+                                    <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+
+                                        <button className="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
+                                            onClick={() => openModal(transaction)}
+                                        >
+                                            View</button>
                                     </td>
 
                                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
@@ -172,6 +177,174 @@ const Show_All_Transactions = () => {
                             ))}
                         </tbody>
                     </table>
+                </div>
+                <div className='max-w-full overflow-x-auto'>
+                    {selectedTransactions && (
+                        <Model
+                            isOpen={isModalOpen}
+                            onRequestClose={closeModal}
+                            contentLabel="Transaction Details"
+                            className="modal"
+                            overlayClassName="overlay"
+                        >
+                            <div className='modelpopupbuttoncontainer'>
+                                <NavLink to={`/transactions/edit_transaction/${selectedTransactions.id}`}>
+                                    <button
+                                        className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 mr-2"
+                                        onClick={closeModal}
+                                        aria-label='edit'
+                                    >
+                                        Edit
+                                    </button>
+                                </NavLink>
+                                <button
+                                    className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 mr-2"
+                                    onClick={closeModal}
+                                    aria-label='close'
+                                >
+                                    Close
+                                </button>
+                            </div>
+                            <div className='modelpopupscrollcountainer'>
+                                <div className='mpdelpopupcontentcontainer'>
+                                    <div className='modelpopupleft'>
+                                        <h1>
+                                            <span style={{ color: 'rgb(112,48,160)' }}>Description</span>
+                                        </h1>
+                                        <p>
+                                            {
+                                                selectedTransactions.description ? (
+                                                    selectedTransactions.description
+                                                ) : (
+                                                    <span>No Description available</span>
+                                                )}
+                                        </p>
+                                    </div>
+                                    <div className='modelpopupright'>
+                                        <table className='w-full'>
+                                            <tbody>
+                                                <tr>
+                                                    <td><strong>amount</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.amount ? (
+                                                                <p className={`inline-flex rounded-full rounded bg-opacity-10 py-1 px-3 text-sm font-medium ${changethebackgrounofthing(selectedTransactions.amount)}`}>
+                                                                    {selectedTransactions.amount}
+                                                                </p>
+                                                            ) : (
+                                                                <span>False</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Second Party</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.second_party ? (
+                                                                <p className={`inline-flex rounded-full rounded bg-opacity-10 py-1 px-3 text-sm font-medium ${changethebackgrounofthing(selectedTransactions.second_party)}`}>
+                                                                    {selectedTransactions.second_party}
+                                                                </p>
+                                                            ) : (
+                                                                <span>No Second Party Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Status</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.status_done ? (
+                                                                <p className={`inline-flex rounded-full rounded bg-opacity-10 py-1 px-3 text-sm font-medium ${changethebackgrounofthing(selectedTransactions.status_done)}`}>
+                                                                    True
+                                                                </p>
+                                                            ) : (
+                                                                <span>False</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Tags</strong></td>
+                                                    <td>
+                                                        {
+                                                            selectedTransactions.tags && selectedTransactions.tags.length > 0 ? (
+                                                                <ul>
+                                                                    {selectedTransactions.tags.map((tag, index) => (
+                                                                        <li key={index} className='inline-block mr-2 bg-grey-200 rounded-full px-2 py-1 text-sm'>
+                                                                            <p className={`inline-flex rounded-full rounded bg-opacity-10 py-1 px-3 text-sm font-medium ${changethebackgrounofthing(tag)}`}>
+                                                                                {tag}
+                                                                            </p>
+                                                                        </li>
+                                                                    ))}
+                                                                </ul>
+                                                            ) : (
+                                                                <span>No tag Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>transaction_type</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.transaction_type ? (
+                                                                <p className={`inline-flex rounded-full rounded bg-opacity-10 py-1 px-3 text-sm font-medium ${changethebackgrounofthing(selectedTransactions.transaction_type)}`}>
+                                                                    {selectedTransactions.transaction_type}
+                                                                </p>
+                                                            ) : (
+                                                                <span>No Transaction Type Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Transaction date</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.transaction_date && selectedTransactions.transaction_date.length > 0 ? (
+                                                                <span>{convertToAmPm(selectedTransactions.transaction_date)}</span>
+
+                                                            ) : (
+                                                                <span>No Transaction date Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>created at</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.created_at && selectedTransactions.created_at.length > 0 ? (
+                                                                <span>{convertToAmPm(selectedTransactions.created_at)}</span>
+
+                                                            ) : (
+                                                                <span>No created at Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>last modified</strong></td>
+                                                    <td className='py-5'>
+                                                        {
+                                                            selectedTransactions.last_modified && selectedTransactions.last_modified.length > 0 ? (
+                                                                <span>{convertToAmPm(selectedTransactions.last_modified)}</span>
+
+                                                            ) : (
+                                                                <span>No last modified Available</span>
+                                                            )
+                                                        }
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </Model>
+                    )}
                 </div>
             </div>
         </>
